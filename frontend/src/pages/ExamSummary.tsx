58d0b698a6b59d7276370a86fac252fcfb869814
PrajwalBagekari <prajwalbagekari555@gmail.com>
2026-08-06 04:05:16 +0530

    8006 exam summay

--- frontend/src/pages/ExamSummary.tsx
+++ frontend/src/pages/ExamSummary.tsx
@@ -26,7 +26,7 @@ export default function ExamSummary() {
 
           const response =
             await axios.get(
-              `https://pdf2exam.org:8006/exam/${id}/questions`
+              `https://pdf2exam.org:/exam/${id}/questions`
             );
 
           setExam({
