1f3c9ffd266ae1b6bfdf3bfd5fc626c805a3207d
PrajwalBagekari <prajwalbagekari555@gmail.com>
2026-08-06 10:53:10 +0530

    Fix postgres host for exam service

--- exam-service/database.py
+++ exam-service/database.py
@@ -7,7 +7,7 @@ import os
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql+psycopg2://postgres:postgres@localhost:5432/examdb"
+    "postgresql://postgres:postgres@postgres:5432/examdb"
 )
 
 
